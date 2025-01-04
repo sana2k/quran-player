e376b53b3e7a1fb9d016534604a5580875fea332
Sana Khan <sany_31@live.com>
2025-01-05 03:28:37 +0400

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
-  basePath: isProd ? "/projects/quran-player" : "",
-  assetPrefix: isProd ? "/projects/quran-player" : "",
-  trailingSlash: true,
 };
 
 export default nextConfig;
