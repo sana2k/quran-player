090251e767672790eeb2ee821418592e7776b811
Sana Khan <sany_31@live.com>
2025-01-05 02:48:53 +0400

    ecosystem

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? "/projects/quran-player" : "",
+  assetPrefix: isProd ? "/projects/quran-player" : "",
 };
 
 export default nextConfig;
