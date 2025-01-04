c4b80b89e582c27e8b1abdee7c79dac737220173
Sana Khan <sany_31@live.com>
2025-01-05 03:17:05 +0400

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
             env: {
                 NODE_ENV: "production",
                 PORT: 3000,
+                BASE_PATH: "/projects/quran-player",
             },
         },
     ],
