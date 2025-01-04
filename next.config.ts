5eed787839b048562d11231a70bb4b5199897d0c
Sana Khan <sany_31@live.com>
2025-01-05 03:33:36 +0400

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  basePath: '/projects/quran-player',
+  assetPrefix: '/projects/quran-player',
+  images: {
+    path: '/projects/quran-player/_next/image'
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
