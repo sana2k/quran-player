40d6f338441f73dd46a3a89ea3ed633a6b003921
Sana Khan <sany_31@live.com>
2025-01-05 03:43:00 +0400

    base path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
   title: "Quran Player with Urdu Translation",
   description: "Listen to Quran recitations with Urdu Translation",
   icons: {
-    icon: '${basePath}/assets/favicon.ico',
-    apple: '${basePath}/assets/logo-blank.png',
+    icon: `${basePath}/assets/favicon.ico`,
+    apple: `${basePath}/assets/logo-blank.png`,
   },
 };
 
