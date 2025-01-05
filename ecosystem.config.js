12c6e93a6b24c217e864ac20dbbb3e781aefada0
Sana Khan <sany_31@live.com>
2025-01-05 04:00:18 +0400

    readme

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
                 NODE_ENV: "production",
                 PORT: 3000,
                 BASE_PATH: "/projects/quran-player",
+                NEXT_PUBLIC_GA_ID: "G-0RTBV0X9PN",
             },
         },
     ],
